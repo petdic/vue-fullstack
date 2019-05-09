c050f9225eb5278ef4183855a7ad7b32aadf3a87
Peter Dicey <peterdicey@gmail.com>
2019-05-09 12:24:33 +0200

    add database connection

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,13 +4,23 @@ const mongodb = require("mongodb");
 const router = express.Router();
 
 // Get Posts
-router.get('/', (req,res) => {
-    res.send("Hello");
+router.get('/', async (req,res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
 });
 
 // Add Post
 
 // Delete Post
 
+
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect(`mongodb+srv://peter123:peter123@cluster0-pyise.mongodb.net/test?retryWrites=true`, {
+        useNewUrlParser: true
+    });
+
+    return client.db('Cluster0').collection('posts');
+}
 
 module.exports = router;
